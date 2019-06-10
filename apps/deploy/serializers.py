bfb95ca4a49413ee30b990a8b7ff88687c462ef1
cainanjie <18058418418@189.cn>
2019-06-10 10:11:24 +0800

    上线申请状态钉钉提醒

--- apps/deploy/serializers.py
+++ apps/deploy/serializers.py
@@ -3,6 +3,8 @@ __datetime__ = '2019/4/26 4:27 PM '
 
 from rest_framework import serializers
 from .models import Project, DeploymentOrder, History
+from common.apis import dingtalk_chatbot
+from .common import D_PENDING, D_REJECTED
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -28,12 +30,29 @@ class ProjectSerializer(serializers.ModelSerializer):
 
         return ret
 
+
 class DeploymentOrderSerializer(serializers.ModelSerializer):
     applicant = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = DeploymentOrder
         fields = '__all__'
         read_only_fields = ['id', 'applicant']
+
+
+    def create(self, validated_data):
+        ret = super(DeploymentOrderSerializer, self).create(validated_data)
+        # 工单创建审核提醒
+        dingtalk_chatbot.text_msg('{}发起了上线申请'.format(ret.applicant.name), at_mobiles=[ret.reviewer.phone])
+        return ret
+
+    def update(self, instance, validated_data):
+        status = validated_data.get('status', instance.status)
+        # 审核状态变更提醒
+        if instance.status == 0 and (status == D_PENDING or status == D_PENDING):
+            dingtalk_chatbot.text_msg('你的上线申请有状态变更', at_mobiles=[instance.applicant.phone])
+        instance.status = status
+        instance.save()
+        return instance
 
     def to_representation(self, instance):
         ret = super(DeploymentOrderSerializer, self).to_representation(instance)
@@ -59,6 +78,8 @@ class DeploymentOrderSerializer(serializers.ModelSerializer):
         }
 
         return ret
+
+
 
 class HistorySerializer(serializers.ModelSerializer):
     class Meta:
