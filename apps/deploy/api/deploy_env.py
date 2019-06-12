e4d183491ac89c70f9d49a9ae4b9d62eeba0cf07
cainanjie <18058418418@189.cn>
2019-06-12 10:42:29 +0800

    deploy env tree

--- apps/deploy/api/deploy_env.py
+++ apps/deploy/api/deploy_env.py
@@ -3,11 +3,13 @@ __datetime__ = '2019/6/11 2:19 PM'
 
 from rest_framework import viewsets, permissions, mixins, status, generics
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from common.utils import logger
 from ..models import DeployEnv
 from ..serializers import DeployEnvSerializer
 from common.pagination import CustomPagination
+
 
 
 
@@ -23,8 +25,33 @@ class DeployEnvViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         # 不需要分页
         if self.request.GET.get('all') == 'true':
-            queryset = self.filter_queryset(self.get_queryset())
-            serializer = self.get_serializer(queryset, many=True)
+            # queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(self.queryset, many=True)
             return Response(serializer.data)
         else:
             return super(DeployEnvViewSet, self).list(request, *args, **kwargs)
+
+
+    @action(detail=False, methods=['get'], name='deploy-env-tree',url_path='tree')
+    def deploy_env_tree(self, request):
+        '''
+        获取部署环境树
+        :param request:
+        :return:
+        '''
+        data = []
+        parents = DeployEnv.objects.filter(parent=None)
+
+        for parent in parents:
+            serializer = self.get_serializer(parent)
+            d = serializer.data
+            children = DeployEnv.objects.filter(parent=parent)
+
+            try:
+                d['children'] = self.get_serializer(children, many=True).data
+            except Exception:
+                d['children'] = []
+
+            data.append(d)
+
+        return Response(data)
