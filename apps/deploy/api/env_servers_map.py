327f8c3be912df1ad1024de3b27486a162eda274
cainanjie <18058418418@189.cn>
2019-06-12 19:16:55 +0800

    deploy

---
+++ apps/deploy/api/env_servers_map.py
@@ -0,0 +1,23 @@
+__author__ = 'singo'
+__datetime__ = '2019/6/11 2:19 PM'
+
+from rest_framework import viewsets, permissions, mixins, status, generics
+from rest_framework.response import Response
+
+from common.utils import logger
+from ..models import EnvServersMap
+from ..serializers import EnvServersMapSerializer
+from common.pagination import CustomPagination
+from ..filters import EnvServersMapFilter
+
+
+class EnvServersMapViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = EnvServersMapSerializer
+    queryset = EnvServersMap.objects.all()
+    pagination_class = CustomPagination
+    filter_class = EnvServersMapFilter
+    search_fields = ('name',)
+    ordering_fields = ('id',)
+
+
